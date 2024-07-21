bb9970894b90b0d90e69175d5d9b06d43a181697
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-07-21 06:54:47 +0530

    web-server

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const fs = require("fs");
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the overview!");
+  } else if (pathName === "/product") {
+    res.end("This is the product");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "/text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("Page not found!");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to requests on port 8000");
+  console.log("<h1>Listening to requests on port 8000</h1>");
 });
